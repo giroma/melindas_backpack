5163d052950cdc27acf13c205e45078fc5e5f9ea
Argir Argirov <dj.giro@gmail.com>
2017-10-31 14:39:27 -0400

    rename things, split up prepare method, factor out repeating variables

--- backpack.rb
+++ backpack.rb
@@ -2,34 +2,37 @@ class Backpack
   def initialize(attributes)
     @attributes = attributes # a hash containing day_of_week and weather keys
     @items = []
-    prepare
+    prepare_weather_items
+    prepare_dayofweek_items
   end
 
   def items
     @items
   end
 
-  def prepare
+  def prepare_weather_items
     # set up local variables used by rest of prepare method
-    x = @attributes[:weather]
-    day_of_week = @attributes[:day_of_week]
+    weather = @attributes[:weather]
+    pants_and_shirt
 
     # Ensure appropriate clothing is added to backpack
-    if x == 'rainy'
-      @items << 'pants'
-      @items << 'shirt'
+    if weather == 'rainy'
       @items << 'umbrella'
-    elsif x == 'cold'
-      @items << 'pants'
-      @items << 'shirt'
+    elsif weather == 'cold'
       @items << 'jacket'
-    else
-      @items << 'pants'
-      @items << 'shirt'
     end
 
+  end
+
+  def pants_and_shirt
+    @items << 'pants'
+    @items << 'shirt'
+  end
+
+  def prepare_dayofweek_items
+    day_of_week = @attributes[:day_of_week]
     # Ensure gym shoes are added to backpack if it's a gym day
-    # Gotta get to the gym on Tuesdays and Thursdays. Wait a sec...
+    # Gotta get to the gym on Monday and Thursdays. Wait a sec...
     if day_of_week == 'monday' || day_of_week == 'thursday'
       #add gym shoes to items
       @items << 'gym shoes'
@@ -38,14 +41,11 @@ class Backpack
     # Bring a packed lunch on all weekdays
     if day_of_week != 'saturday' && day_of_week != 'sunday'
       @items << 'packed lunch'
-    elsif false
-      # Used to bring snacks on weekend trips, but now I just buy 'em
-      @items << 'snacks'
+
     end
   end
-
   # Prints a summary packing list for Melinda's backpack
-  def my_func
+  def print_list
     output = []
     output << "Melinda, here's your packing list!"
     output << "Day: #{@attributes[:day_of_week]}, Weather: #{@attributes[:weather]}"
--- backpack_test.rb
+++ backpack_test.rb
@@ -71,7 +71,7 @@ Day: monday, Weather: sunny
 - shirt
 - gym shoes
 - packed lunch"
-    assert_equal(backpack.my_func, expected)
+    assert_equal(backpack.print_list, expected)
   end
 
 end
